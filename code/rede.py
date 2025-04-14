55b96d87ca03018bf1c5b1ead3cce3c8f8737ee4
joao506paulo <joao506paulo@gmail.com>
2025-04-14 19:17:21 -0300

    atualização

--- code/rede.py
+++ code/rede.py
@@ -6,13 +6,28 @@ Neuronios_Camada_Escondida = 2
 Neuronios_Saida = 1
 theta = 2
 
-def neuronio(Pesos_camada_escondida, x_i):
-    
-    y_in = Pesos_camada_escondida[0]*x_i[0] + Pesos_camada_escondida[1]*x_i[1]
-    if(y_in > theta):
+#função de ativação, pode (e deve ser mudada)
+#recebe y_in calculado pelo neuronio e devolve o resultado da função de ativação
+def funcao_ativacao(y_in):
+    y = 0
+    if(y_in >= theta):
         y = 1
-    elif(y_in < theta and y_in > theta*(-1)):
-        y = 0
-    elif (y_in < theta*(-1)):
+    #elif(y_in < theta and y_in > theta*(-1)):
+        #y = 0
+    elif (y_in < theta):
         y = -1
-    print(y)
+    return y
+
+#recebe uma lista de pesos e entradas e seu bias e retorna o resultado da função de ativação
+def neuronio(numero_de_entradas, Pesos, entradas, bias):
+    y_in = bias
+    for i in range(numero_de_entradas):
+        y_in = y_in + Pesos[i]*entradas[i]
+    return funcao_ativacao(y_in)
+
+#recebe uma lista de pesos e entradas para cada neurônio e seu bias, e o número de neurônios e returna uma lista com o resultado de cada neurônio
+def camada_da_rede(numero_de_neuronios, Pesos_neuronios, entradas_neuronios, bias_neuronios):
+    resultado = []
+    for i in range(numero_de_neuronios):
+        resultado.append(neuronio(len(entradas_neuronios), Pesos_neuronios[i], entradas_neuronios, bias_neuronios[i]))
+    return resultado
---
+++ code/teste.py
@@ -0,0 +1,3 @@
+from rede import camada_da_rede, neuronio
+
+print(camada_da_rede(2, [[2, 2], [2, 2]], [1, 1], [0, 0]))
